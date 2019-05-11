27304c87487fcb7a495c1ce35863c5873e81b148
Caleb McBride <atfbassman@yahoo.com>
2019-05-10 18:46:24 -0600

    finished 1-3

--- nesting.js
+++ nesting.js
@@ -50,8 +50,16 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
-
+function employeeUpdater(){
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].firstName === 'Theo') { 
+      delete employees[i]
+  }  else if (employees[i].firstName === 'Lorie') {
+    employees[i].department = 'HR'
+  }  
+}
+return employees
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -68,7 +76,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+function removeDuplicates(){
+  for (var i = 0; i< workplaceAccidents.length; i++) {
+    for (var j = i + 1; j< workplaceAccidents.length; j++) {
+      if (workplaceAccidents[i] === workplaceAccidents[j]) {
+        workplaceAccidents.splice(j, 1)
+        j--
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 
@@ -97,8 +115,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
